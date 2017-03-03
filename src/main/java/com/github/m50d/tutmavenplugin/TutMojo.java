23022c70ce4c26c8c5930ca65892d782cfd2ae8f
Michael Donaghy <md401@srcf.ucam.org>
2017-03-03 17:10:13 +0000

    Add invocation of tut

--- src/main/java/com/github/m50d/tutmavenplugin/TutMojo.java
+++ src/main/java/com/github/m50d/tutmavenplugin/TutMojo.java
@@ -5,8 +5,11 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import tut.TutMain;
+
 @Mojo(name="tut")
 public class TutMojo extends AbstractMojo {
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		TutMain.main(new String[]{});
 	}
 }
