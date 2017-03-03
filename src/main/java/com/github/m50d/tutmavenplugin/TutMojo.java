b44533f96403439899c4a91fb745c74759875c6d
Michael Donaghy <md401@srcf.ucam.org>
2017-03-03 16:57:07 +0000

    Create mojo so that this builds

---
+++ src/main/java/com/github/m50d/tutmavenplugin/TutMojo.java
@@ -0,0 +1,12 @@
+package com.github.m50d.tutmavenplugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name="tut")
+public class TutMojo extends AbstractMojo {
+	public void execute() throws MojoExecutionException, MojoFailureException {
+	}
+}
